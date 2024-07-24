060b809a49fc1222c3c91b585bd2a718b5c9ff86
jhernandezj <11543138+jhernandezj@users.noreply.github.com>
2024-07-24 12:28:58 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,16 +34,21 @@ data "aws_ami" "amazon_linux_2" {
 # ---------------------------------------------------------
 # Estado remoto
 # ---------------------------------------------------------
-data "terraform_remote_state" "remote_state" {
-  # Define the backend configuration for the remote state (required argument)
-  backend {
-    type = "s3"
+terraform {
+  backend "s3" {
     bucket = "tf-state-pharos-269433206282-eu-west-1"
     key    = "aws_vpc_tonta/Structuralia/dev/vptonta/terraform.tfstate"
+    region = "eu-west-1"
   }
+}
 
-  # Specify the path to the desired output within the remote state (optional argument)
-  path = "outputs.public_subnets.value"
+data "terraform_remote_state" "remote_state" {
+  backend = "s3"
+  config = {
+    bucket = "tf-state-pharos-269433206282-eu-west-1"
+    key    = "aws_vpc_tonta/Structuralia/dev/vptonta/terraform.tfstate"
+    region = "eu-west-1"
+  }
 }
 
 # ---------------------------------------------------------
@@ -52,7 +57,7 @@ data "terraform_remote_state" "remote_state" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.amazon_linux_2.id
   instance_type = "t3a.micro"
-  subnet_id     = data.terraform_remote_state.remote_state.value  # Access public subnets from remote state
+  subnet_id     = data.terraform_remote_state.remote_state.outputs.public_subnets[0]  # Accede a la primera subred pública del estado remoto
 
   tags = {
     Name = "instancia-tonta"
